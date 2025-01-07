fa71147a134845a19278a59b8e187827387acda3
Ritika-S30 <ritikas.22eie@kongu.edu>
2025-01-07 11:23:55 +0530

    done

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Apps from './back/Apps.jsx';
 
-//import Chicken from './Prop/chicken.jsx'
+
 //import Hooks from './Hooks.jsx'
-//import Chicken from './Prop/chicken'
+//import Chicken from './Prop/Chicken'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
